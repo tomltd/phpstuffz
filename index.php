f76ee8e42aa385310c14d507f9eacf66c599a4c8
Tom Freestone <tomfreestone@gmail.com>
2016-10-03 20:39:50 +0100

    some layout stuff

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$person = [
+        'age' => 31,
+        'hair' => 'blond',
+        'job' => 'web developer'
+    ];
+
+    $person['name'] = 'Tom';
+
+
+
+
+//var_dump($person);
+
+$task = [
+    'title' => 'Finish Homework',
+    'due' => 'today',
+    'assigned_to' => 'Tom',
+    'completed' => false
+    ];
+
+//var_dump($task);
+
+
+//    echo $take['complete'] ? 'It is complete' : 'incomplete';
+
+
+//    echo 'Due Date ' . $task['due'];
+
+    ;
+
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en-GB">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Hello</h1>
+
+    <ul>
+        <?php foreach ($person as $key => $feature) : ?>
+            <li><strong><?= $key ?></strong>:<?= $feature ?>
+        <?php endforeach ?>
+    </ul>
+
+
+</body>
+
+</html>
