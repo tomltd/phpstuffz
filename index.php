23552f6169988f14e9ff9f34df179a06ce9b6cd2
Tom Freestone <tomfreestone@gmail.com>
2016-10-10 12:16:14 +0000

    refine the router@

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 
 $database = require 'core/bootstrap.php';
 
-$router = new Router;
-
-require 'routes.php';
+// $router = new Router;
+//
+// require 'routes.php';
 
 $uri = trim($_SERVER['REQUEST_URI'], '/');
+
+// require $router->direct($uri);
+
+$router = Router::load('routes.php');
 
 require $router->direct($uri);
 
