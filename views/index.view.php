6288bf524270c08b32c5dd9155a4fc79e68a0277
Tom Freestone <tomfreestone@gmail.com>
2016-10-10 14:45:54 +0000

    array_map

--- views/index.view.php
+++ views/index.view.php
@@ -15,4 +15,53 @@
 
     </ul>
 
+<?php
+
+class Post
+{
+    public $title;
+    public $published;
+
+    public function __construct($title, $published)
+
+    {
+        $this->title = $title;
+        $this->published = $published;
+    }
+}
+
+$posts = [
+        new Post('My First Post', true),
+        new Post('My Second Post', true),
+        new Post('My Third Post', true),
+        new Post('My Fourth Post', false),
+
+    ];
+
+    $modified = array_map(function($post) {
+        $post->published = true;
+        return $post;
+    }, $posts);
+
+var_dump($modified);
+
+//var_dump($posts);
+
+// $unpublishedPosts = array_filter($posts, function($post) {
+//     return ! $post->published;
+// });
+//
+// var_dump($unpublishedPosts);
+//
+// $publishedPosts = array_filter($posts, function($post) {
+//     return $post->published;
+// });
+
+// var_dump($publishedPosts);
+
+
+
+
+?>
+
     <?php require('partials/footer.php'); ?>
