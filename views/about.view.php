288ef74e153183d4f1ba5f60df4f77a116d485e0
Tom Freestone <tomfreestone@gmail.com>
2016-10-10 13:00:49 +0000

    set up templates

--- views/about.view.php
+++ views/about.view.php
@@ -1,20 +1,8 @@
-
-<!DOCTYPE html>
-<html dir="ltr" lang="en-GB">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</head>
+<?php require('partials/header.php'); ?>
 
 <body>
     <h1>About</h1>
 
 
 
-
-</body>
-
-</html>
+    <?php require('partials/footer.php'); ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,15 +1,6 @@
+<?php require('partials/header.php'); ?>
 
-<!DOCTYPE html>
-<html dir="ltr" lang="en-GB">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-</head>
-
-<body>
     <h1>Hello</h1>
     <ul>
         <?php foreach ($tasks as $task) : ?>
@@ -24,8 +15,4 @@
 
     </ul>
 
-
-
-</body>
-
-</html>
+    <?php require('partials/footer.php'); ?>
